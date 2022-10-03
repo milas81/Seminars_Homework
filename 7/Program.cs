4ae025440a57395619bc321d4bb1270cd46a750e
liudmila <milas81@mail.ru>
2022-10-03 18:49:57 +0300

    Task #7

---
+++ 7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число, а на выходе показывает последнюю цифру данного числа
+
+Console.Clear();
+Console.WriteLine("Put a three-digit number:");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 99 && number < 1000)
+{
+Console.WriteLine(number % 10);
+}
+else Console.WriteLine("ERROR");
+

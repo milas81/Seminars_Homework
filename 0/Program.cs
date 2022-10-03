52d339f9a88ca97c8cdbb63ba33fd9756447e68a
liudmila <milas81@mail.ru>
2022-10-03 16:20:09 +0300

    Square of number - task #0

---
+++ 0/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число
+// и выдает его квадрат (число умноженное на само себя).
+
+Console.Clear();
+Console.Write("put the number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+Console.WriteLine($"Square of the number {number} is equal to {sqr}");

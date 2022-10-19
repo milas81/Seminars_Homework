5ea99cf13b1202d90d8ba5e678eddea7f20d4bd3
liudmila <milas81@mail.ru>
2022-10-19 16:08:02 +0300

    Homework seminar 5

---
+++ HomeworkS5/Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на
+//нечётных позициях. [3, 7, 23, 12] -> 19             [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetSummOdd(int[] numArray)
+{
+    int Summ = 0;
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Summ = Summ + numArray[i];
+        }
+
+    }
+    return Summ;
+}
+
+
+Console.Clear();
+int[] array = GetArray(4, -100, 100);
+Console.WriteLine(String.Join(", ", array));
+Console.Write($"Сумма нечетных элементов массиве {GetSummOdd(array)}.");
---
+++ HomeworkS5/Task38/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива
+// [3 7 22 2 78] -> 76
+
+double[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    double[] res = new double[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = double.Parse(nums[i]);
+    }
+    return res;
+}
+
+/*double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble()*(maxValue - minValue) + minValue;
+    }
+    return res;
+}*/
+
+double FindMax(double [] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]> max)
+        {
+            max = array[i];
+        } 
+    }
+    return max;
+}
+
+double FindMin(double [] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]< min)
+        {
+            min = array[i];
+        } 
+    }
+    return min;
+}
+
+Console.Clear();
+Console.WriteLine("Введите массив вещественных чисел через пробел:");
+double[] array = GetArrayFromString(Console.ReadLine());
+Console.WriteLine(String.Join("; ", array));
+Console.WriteLine(FindMax(array)- FindMin(array));
+

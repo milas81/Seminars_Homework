778d2c601dcd757e7e72979f74dad6d7f2f6ca75
liudmila <milas81@mail.ru>
2022-11-17 17:53:05 +0300

    HomeworkS9

---
+++ HomeworkS9/Test64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напищите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//Выполнить с помощью рекурсии.
+
+string GetPrint(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return start + " " + GetPrint(start -1, end);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(GetPrint(n, m));
---
+++ HomeworkS9/Test66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+int GetSumm (int start, int end)
+{
+    //int summ = 0;
+    if (start == end)
+    {
+        return end;
+    }
+    return start + GetSumm(start + 1, end);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+if (m > n)
+{
+    Console.WriteLine("Введен неправильный промежуток чисел");
+}
+else Console.WriteLine($"Сумма чисел в промежутке {m} и {n} равна: {GetSumm(m, n)}.");
